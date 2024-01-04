bc1f195408e7c8018a9543f9006768cb53d32b24
Jeferson <jefemarcossbs@gmail.com>
2024-01-03 21:09:27 -0300

    Criado index.html

--- criptografia/views.py
+++ criptografia/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template_index = loader.get_template('index.html')
+    return HttpResponse(template_index.render())
