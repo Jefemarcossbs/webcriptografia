afb91c465ae2ffec03a3378949f3051d4c22a6d6
Jeferson <jefemarcossbs@gmail.com>
2024-01-13 19:58:58 -0300

    geração de chave e download ok

--- criptografia/views.py
+++ criptografia/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .forms import Gerar_Chave
 from .models import new_key
 import os
+from django.conf import settings
 # Create your views here.
 def index(request):
     form = Gerar_Chave()
@@ -16,16 +17,19 @@ def gerar_chave(request):
         generate_new_key = new_key.generate_keys(chaveform)
         
         ##retornar o dowload da chave
-        fl_path = '{}{}'.format(chaveform,'.key')
-        fl_download = open(fl_path, 'r')
-        mime_type, _ = mimetypes.guess_type(fl_path)
-        response = HttpResponse(fl_download, content_type=mime_type)
-        response['Content-Disposition'] = "attachment; filename=%s" % fl_download
+        #fl_path = '{}{}'.format(chaveform,'.key')
+        fl_path = os.path.join(settings.MEDIA_ROOT, '{}{}'.format(chaveform,'.key'))
+        with open(fl_path, 'rb') as fl_download:
+            mime_type, _ = mimetypes.guess_type(fl_path)
+            response = HttpResponse(fl_download.read(), content_type=mime_type)
+            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(fl_path)
+            return response
+        #mime_type, _ = mimetypes.guess_type(fl_path)
+        
         #-filename = ‘downloaded_file_name.extension’
         #-fl = open(fl_path, 'r’)
         #-mime_type, _ = mimetypes.guess_type(fl_path)
         #-response = HttpResponse(fl, content_type=mime_type)
         #response['Content-Disposition'] = "attachment; filename=%s" % filename
         #return response
-    return HttpResponse(response)
     
