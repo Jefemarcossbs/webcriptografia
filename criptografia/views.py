dc10c6305c43f596811731084109988287d7c6aa
Jeferson <jefemarcossbs@gmail.com>
2024-01-13 14:34:37 -0300

    função gerar chave ok

--- criptografia/views.py
+++ criptografia/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.template import loader
 from .forms import Gerar_Chave
-import models
+from .models import new_key
 # Create your views here.
 def index(request):
     form = Gerar_Chave()
@@ -13,7 +11,18 @@ def gerar_chave(request):
     form = Gerar_Chave(request.POST)
     if form.is_valid:
         chaveform = form['nome_chave'].value()
-        chave = models.generate_keys(chaveform)
-        print(form['nome_chave'].value())
-    return HttpResponse(chave)
+        generate_new_key = new_key.generate_keys(chaveform)
+       
+
+        ##retornar o dowload da chave
+        #fl_path = ‘/file/path'
+        #filename = ‘downloaded_file_name.extension’
+        #fl = open(fl_path, 'r’)
+        #mime_type, _ = mimetypes.guess_type(fl_path)
+        #response = HttpResponse(fl, content_type=mime_type)
+        #response['Content-Disposition'] = "attachment; filename=%s" % filename
+        #return response
+
+        print(generate_new_key)
+    return HttpResponse(chaveform)
     
